eb8baae5dbcbed66490108ad2e4984097b9dadb0
Luis Pedro Lira <luispelirak130@gmail.com>
2024-08-19 21:02:16 -0600

    Content of counter header

--- app/src/main/java/com/luispedrolira/plataformasmoviles/lab6/Contador.kt
+++ app/src/main/java/com/luispedrolira/plataformasmoviles/lab6/Contador.kt
@@ -1,33 +1,103 @@
 package com.luispedrolira.plataformasmoviles.lab6
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.luispedrolira.plataformasmoviles.ui.theme.PlataformasMovilesTheme
 
 @Composable
 fun CounterScreen(
     modifier: Modifier = Modifier
 ){
-    Counter()
-}
+    Column(modifier = modifier) {
+        CounterHeader()
+        CounterStadistics()
+        CounterFooter()
+    }
 
-@Composable
-private fun Counter(
-    modifier: Modifier = Modifier
-){
-    CounterHeader()
-    CounterStadistics()
-    CounterFooter()
 }
 
 @Composable
 private fun CounterHeader(
     modifier: Modifier = Modifier
 ){
+    //var contador_string = ""
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Luis Pedro Lira",
+            style = MaterialTheme.typography.displaySmall
+        )
+        Row (
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape
+                    )
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+
+            Text(
+                text = "5",
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape
+                    )
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+
+        }
+    }
 }
 
 @Composable
@@ -48,8 +118,8 @@ private fun CounterFooter(
 @Preview
 @Composable
 private fun PreviewCounterScreen() {
-    Surface {
-        PlataformasMovilesTheme {
+    PlataformasMovilesTheme {
+        Surface {
             CounterScreen(modifier = Modifier.fillMaxSize())
         }
     }
