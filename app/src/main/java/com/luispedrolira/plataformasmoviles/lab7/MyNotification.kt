e41b4ef84cbf3ef84e909d025538fdbc3cbfc17f
Luis Pedro Lira <luispelirak130@gmail.com>
2024-08-28 12:17:40 -0600

    MyNotification Structure

--- app/src/main/java/com/luispedrolira/plataformasmoviles/lab7/MyNotification.kt
+++ app/src/main/java/com/luispedrolira/plataformasmoviles/lab7/MyNotification.kt
@@ -1,2 +1,38 @@
 package com.luispedrolira.plataformasmoviles.lab7
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.luispedrolira.plataformasmoviles.ui.theme.PlataformasMovilesTheme
+
+
+@Composable
+fun MyNotificationScreen(
+    modifier: Modifier = Modifier
+){
+
+}
+
+
+@Composable
+private fun NotificationType(){
+
+}
+
+@Composable
+private fun NotificationSection(){
+
+}
+
+@Preview
+@Composable
+private fun PreviewMyNotificationScreen() {
+    Surface {
+        PlataformasMovilesTheme {
+            MyNotificationScreen(modifier = Modifier.fillMaxSize())
+        }
+    }
+}
